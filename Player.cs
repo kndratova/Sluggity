71595b69f1995259b03c8291acd900fed7948307
kndratova <katiakondratova@gmail.com>
2024-04-23 03:08:44 +0300

    its jumping and colliding needs refactor still

--- Player.cs
+++ Player.cs
@@ -1,39 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Diagnostics;
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Media3D;
 using System.Windows.Shapes;
 
 namespace Sluggity
 {
-    public enum Direction
-    {
-        Up, Down, Left, Right
-    }
-
     public class Player : INotifyPropertyChanged
     {
         private Point _position;
-        private float _moveSpeed = 5;
-        private float _gravityScale = 9;
-        private Point _previousPosition;
-        private float _jumpVelocity;
+        private float _moveSpeed = 4;
+        private float _gravityScale = 1f;
+        private float _jumpForce = 16;
+        private float _verticalVelocity = 0f;
+        private bool _isOnGround = false;
+        private bool _upKeyReleased;
         private Rectangle _playerRectangle;
-        private bool _isJumping = false; // Flag to track if the player is currently jumping
-        private int _jumpTimer = 0;
-
-        private Canvas GameCanvas;
         private List<Rectangle> obstacles = new List<Rectangle>();
 
-        public Player(Canvas GameCanvas, Rectangle playerRectangle)
+        public Player(Canvas gameCanvas, Rectangle playerRectangle)
         {
-            foreach (var rectangle in GameCanvas.Children.OfType<Rectangle>())
+            foreach (var rectangle in gameCanvas.Children.OfType<Rectangle>())
             {
                 if ((string)rectangle.Tag == "Obstacle")
                 {
@@ -41,7 +31,6 @@ namespace Sluggity
                 }
             }
             _playerRectangle = playerRectangle;
-            this.GameCanvas = GameCanvas;
         }
 
         public Point Position
@@ -57,79 +46,53 @@ namespace Sluggity
             }
         }
 
-        public void ReturnToPreviousPosition()
-        {
-            Position = _previousPosition;
-        }
-
         public void Move()
         {
-            Point newPosition = Position;
+            ApplyGravity(); // Apply gravity before moving
+            Point newPosition = Position; // Store the new position initially as the current position
 
-            // Horizontal movement
             if (Keyboard.IsKeyDown(Key.Left))
             {
-                newPosition = new Point(Position.X - _moveSpeed, Position.Y);
-                while (IsCollision(newPosition))
-                {
-                    newPosition.X++; // Move left until no collision
-                }
+                newPosition = new Point(newPosition.X - _moveSpeed, newPosition.Y);
             }
             if (Keyboard.IsKeyDown(Key.Right))
             {
-                newPosition = new Point(Position.X + _moveSpeed, Position.Y);
-                while (IsCollision(newPosition))
-                {
-                    newPosition.X--; // Move right until no collision
-                }
+                newPosition = new Point(newPosition.X + _moveSpeed, newPosition.Y);
+            }
+            if (Keyboard.IsKeyDown(Key.Up) && _isOnGround && _upKeyReleased)
+            {
+                _upKeyReleased = false;
+                Jump();
             }
 
-            // Vertical movement (jumping)
-            if (Keyboard.IsKeyDown(Key.Space) && !_isJumping) // Check if not already jumping
+            if (Keyboard.IsKeyUp(Key.Up)) _upKeyReleased = true;
+
+            if (!IsCollision(newPosition))
             {
-                _isJumping = true;
-                _jumpTimer = 20; // Set the jump duration (adjust as needed)
-                _jumpVelocity = -20; // Reset jump velocity
+                Position = newPosition;
             }
+        }
 
-            if (_isJumping)
+        private void ApplyGravity()
+        {
+            // Apply vertical velocity due to gravity
+            _verticalVelocity += _gravityScale;
+            Point newPosition = new Point(Position.X, Position.Y + _verticalVelocity);
+            if (!IsCollision(newPosition))
             {
-                Point jumpPosition = new Point(newPosition.X, Position.Y + _jumpVelocity);
-                while (IsCollision(jumpPosition))
-                {
-                    jumpPosition.Y++; // Move upwards until no collision
-                }
-
-                newPosition = jumpPosition;
-                _jumpVelocity++; // Apply gravity to the jump
-                _jumpTimer--;
-
-                if (_jumpTimer <= 0 || _jumpVelocity >= 0)
-                {
-                    _isJumping = false; // Reset jump flag when jump ends or reaches maximum height
-                }
+                _isOnGround = false;
+                Position = newPosition;
             } else
             {
-                // Apply gravity
-                newPosition = new Point(newPosition.X, Position.Y + _gravityScale);
-                while (IsCollision(newPosition))
-                {
-                    newPosition.Y--; // Move downwards until no collision
-                }
+                _isOnGround = true;
+                _verticalVelocity = 0;
             }
-
-            Position = newPosition;
-        }
-        public void AdjustGravity()
-        {
-            Position = new Point(Position.X, Position.Y + _gravityScale);
         }
 
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        protected virtual void OnPropertyChanged(string propertyName)
+        private void Jump()
         {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            // Apply upward force to initiate jump
+            _verticalVelocity = -_jumpForce;
         }
 
         private bool IsCollision(Point newPosition)
@@ -149,5 +112,11 @@ namespace Sluggity
             return false;
         }
 
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
